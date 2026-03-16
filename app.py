0d0d1c7cc648df3175e321c81b729125fec65ee3
irize104-bit <irize104@gmail.com>
2026-03-16 18:49:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -211,8 +211,11 @@ with app.app_context():
     db.create_all()
 
 # Run App
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
